c7f37f8ace74ae17f2d774991b0f7837cb736a15
Александр <alo0606@mail.ru>
2022-08-09 19:47:44 +0500

    Основа приложения

--- SearchAndAutomation/ViewModels/MainWindowViewModel.cs
+++ SearchAndAutomation/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using Prism.Mvvm;
+using Prism.Commands;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+
 
 namespace SearchAndAutomation.ViewModels
 {
@@ -11,8 +15,65 @@ namespace SearchAndAutomation.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        private BitmapImage _currentImage = new BitmapImage();
+        public BitmapImage CurrentImage
+        {
+            get { return _currentImage; }
+            set { SetProperty(ref _currentImage, value); }
+        }
+
+        private Process _currentProcess = new Process();
+        public Process CurrentProcess
+        {
+            get { return _currentProcess; }
+            set { SetProperty(ref _currentProcess, value); }
+        }
+        #region Commands
+        
+        public DelegateCommand<object> SelectProcessCommand { get; private set; }
+
+        void OnSelectProcess(object p)
+        {
+            if(p as string =="") { return; }
+
+            var procId = int.Parse(p as string);
+            try
+            {
+                CurrentProcess = Process.GetProcessById(procId);
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+
+        bool CanSelectProcess(object p) => true;
+
+
+
+        public DelegateCommand<object> RefreshImageCommand { get; private set; }
+
+        void OnRefreshImage(object p)
+        {
+            
+        }
+
+        bool CanRefreshImage(object p) 
+        {
+            if (_currentProcess != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
         public MainWindowViewModel()
         {
+
+            SelectProcessCommand = new DelegateCommand<object>(OnSelectProcess, CanSelectProcess);
 
         }
     }
