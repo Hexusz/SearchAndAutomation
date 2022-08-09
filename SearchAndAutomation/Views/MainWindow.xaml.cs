16d95f83101a162f4256e2c4c6466811cb3a6dca
Александр <alo0606@mail.ru>
2022-08-09 16:26:34 +0500

    Add project files.

---
+++ SearchAndAutomation/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SearchAndAutomation.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Wpf.Ui.Controls.UiWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
